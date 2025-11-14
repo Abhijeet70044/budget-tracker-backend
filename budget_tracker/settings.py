8778aad425e592b5916db355e577f02aedd08fdc
Abhijeet <abhijeetkumar70044@gmail.com>
2025-11-14 18:21:54 +0530

    add: start.sh

--- budget_tracker/settings.py
+++ budget_tracker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s1n54_z0@9j&wzu@&pf2rro6nh1(!6kyxas_euahy6xmmyq-x('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = "*"
 
 
 # Application definition
