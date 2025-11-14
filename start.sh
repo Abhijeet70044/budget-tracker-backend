bffd6795749d5676b06899c5c213adca05022103
Abhijeet <abhijeetkumar70044@gmail.com>
2025-11-14 18:40:20 +0530

    update requirements.txt

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Start Django app with Gunicorn on port 8000 or $PORT if set, binding to all addresses
-PORT=${PORT:-8000}
-exec gunicorn budget_tracker.wsgi:application --bind 0.0.0.0:$PORT --workers 3
